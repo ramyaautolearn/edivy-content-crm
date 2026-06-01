063a4a02e3a24b0b05a928fa6349801d6117ab17
ramyaautolearn <ramya.autolearn@gmail.com>
2026-06-01 23:19:42 +0530

    fixed tsconfig build errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { onAuthStateChanged, signOut } from 'firebase/auth';
 import { collection, getDocs } from 'firebase/firestore';
 import { auth, db } from './firebase';
